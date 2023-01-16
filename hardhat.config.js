ee1f97666f858611a5505f66f3fece7cfab44206
Jay M Gonzalez <jaimungon@gmail.com>
2023-01-16 17:29:55 +0100

    fix issues with v2 contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,9 @@ require('dotenv').config()
 
 module.exports = {
   solidity: '0.8.17',
+  defaultNetwork: 'localhost',
   networks: {
+    localhost: {},
     goerli: {
       url: process.env.ALCHEMY_GOERLI_URL,
       accounts: [process.env.GOERLI_PRIVATE_KEY],
--- scripts/updateProxy.js
+++ scripts/updateProxy.js
@@ -1,6 +1,9 @@
 const { ethers, upgrades } = require('hardhat')
-
+// GOERLI
 const proxyAddress = '0x4C8603dC88D53ff2E181A505f8B4B9C3f0be726c'
+
+// HARDHAT
+// const proxyAddress = '0x22753E4264FDDc6181dc7cce468904A80a363E44'
 
 async function main() {
   const VendingMachineV2 = await ethers.getContractFactory('VendingMachineV2')
@@ -10,8 +13,12 @@ async function main() {
     proxyAddress
   )
 
-  console.log('The current contract owner is: ' + upgraded.owner())
+  // console.log(await upgraded.owner())
+
+  console.log('The current contract owner is: ' + (await upgraded.owner()))
   console.log('Implementation contract address: ' + implementationAddress)
 }
 
 main()
+
+// Implementation contract address: 0x97E2aDD862DD2d1679A61D1D7B70aA54220536eC
